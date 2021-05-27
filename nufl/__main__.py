405635ab53932430e815add40ea22601b8c437c0
hark130 <hark130@yahoo.com>
2021-05-26 23:25:50 -0500

    development: main.py update
    Basic functionality exists
    
    Changes to be committed:
    	modified:   nufl/__main__.py

--- nufl/__main__.py
+++ nufl/__main__.py
@@ -1,12 +1,19 @@
-from nufl.nufldb import NuflDb
+# Standard Imports
 import os
+import sys
+
+# Third Party Imports
+
+# Local Imports
+from nufl.nufldb import NuflDb
+from nufl.nufl_menu import run_main_menu
 
 
 def main() -> None:
-    print('NUFL!')
-    # parse_db(os.path.join('raw', 'working.db'))
-    nufl_obj = NuflDb(os.path.join('raw', 'working.db'))
+    nufl_obj = NuflDb(sys.argv[1])
     nufl_obj.parse()
+    run_main_menu(nufl_obj)
+    # parse_db(os.path.join('raw', 'working.db'))
     # for entry in nufl_obj._select_statement('*', 'sqlite_master'):
     #     print(entry)
     # record = nufl_obj._get_record('Team_Listing', '*', 2)
@@ -15,9 +22,13 @@ def main() -> None:
     # print(entry)
     # entry = nufl_obj._get_record_index('Team_Listing', '*', 2, 7)
     # print(entry)
-    print(nufl_obj.get_team_name(362))
-    print(nufl_obj.get_team_num('Bearded Basterds'))
-    print(nufl_obj.get_team_cash('Bearded Basterds'))
+    # print(nufl_obj.get_team_name(362))
+    # print(nufl_obj.get_team_num('Bearded Basterds'))
+    # print(nufl_obj.get_team_cash('Bearded Basterds'))
+    # nufl_obj._update_statement('Team_Listing', 'iCash', 'ID', 1337, 362)
+    # print(nufl_obj.get_team_cash('Bearded Basterds'))
+    # nufl_obj.commit()
+
 
 if __name__ == '__main__':
     main()
